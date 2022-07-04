ae41f8e775e9809ea355de9840720ed5f1b7f383
FranckyPoop <afrifranck2003@gmail.com>
2022-07-04 12:05:05 +0000

    Fin de la création des models de l'application front

--- front/models.py
+++ front/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 class WelcomeText(models.Model):
-    mini_title = models.CharField(max_length=150)
+    mini_title = models.CharField(max_length=150, error_messages={"invalid": "tu et bête comment entre un caratére laba"})
     main_title = models.CharField(max_length=150)
     description = models.TextField()
     link = models.URLField()
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
     
 class VideoWelcome(models.Model):
-    # faire une recherche sur quell type de champ pour les videos
+    video = models.FileField() 
     
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
 
 class Banner(models.Model):
     image = models.ImageField()
@@ -25,23 +25,50 @@ class Banner(models.Model):
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
 
 class MeetingCategory(models.Model):
     name = models.CharField(max_length=150)
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
 
 class Meeting(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
+    price = models.PositiveIntegerField()
+    photo = models.ImageField()
+    #meeting_category = models.ForeignKey(MeetingCategory, on_delete=models.CASCADE)
+    location = models.TextField()
+    book_now = models.TextField()
     date_meeting = models.DateTimeField()
+    
+    monday = models.DateTimeField(blank=True, null=True)
+    tuesday = models.DateTimeField(blank=True, null=True)
+    Wednesday  = models.DateTimeField(blank=True, null=True)
+    Thursday  = models.DateTimeField(blank=True, null=True)
+    Friday  = models.DateTimeField(blank=True, null=True)
+    saturday = models.DateTimeField(blank=True, null=True)
+    sunday = models.DateTimeField(blank=True, null=True)
+    
+    link_facebbok = models.URLField(blank=True, null=True)
+    link_twitter = models.URLField(blank=True, null=True)
+    link_instagram = models.URLField(blank=True, null=True)
+    link_linkedin = models.URLField(blank=True, null=True)
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
+    
+class BoxBachelor(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    link = models.URLField()
+    
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.BooleanField(default=True)
 
 class Faq(models.Model):
     name = models.CharField(max_length=150)
@@ -49,17 +76,17 @@ class Faq(models.Model):
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
 
 class PopularCourses(models.Model):
+    # faire une recherche sur comment recuperer les etoiles des avis
     title = models.CharField(max_length=150)
     price = models.PositiveIntegerField()
-    # faire une recherche sur comment recuperer les etoiles des avis
     image = models.ImageField()
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
     
 class UniversityStatistic(models.Model):
     name = models.CharField(max_length=150)
@@ -67,19 +94,23 @@ class UniversityStatistic(models.Model):
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
     
 class Video(models.Model):
+    url_link_youtube = models.URLField(blank=True, null=True)
+    video = models.FileField(blank=True, null=True)
+    
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
     
 class SiteContact(models.Model):
     phone = PhoneNumberField()
     email = models.EmailField(max_length=254)
     address = models.CharField(max_length=150)
     website = models.URLField()
+    _copyright = models.CharField(max_length=150, verbose_name="copyright") 
     
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now_add=True)
+    date_deleted = models.BooleanField(default=True)
